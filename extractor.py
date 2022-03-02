87c1f8ebdbe9ef96e1e2921527766b27db4312e2
unknown <suncheture@gmail.com>
2022-03-02 23:44:17 +0300

    Rename file extraction module Issue #2 Изменено имя файла

---
+++ extractor.py
@@ -0,0 +1,35 @@
+import requests
+import os
+from bs4 import BeautifulSoup as bs
+from urllib.parse import urljoin
+
+from constants import TEMP_DOWNLOADS_PATH
+
+def download_static_files(url: str) -> None:
+    session = requests.Session()
+    session.headers["User-Agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+    html = session.get(url).content
+
+    soup = bs(html, "html.parser")
+
+    files_to_download = list()
+
+    # get the file links
+    for file in soup.find_all(["script", "link"]):
+        #JavaScript
+        if file.attrs.get("src"):
+            file_url = urljoin(url, file.attrs.get("src"))
+            files_to_download.append(file_url.split("?")[0])
+        #CSS
+        if file.attrs.get("href") and file.attrs.get("rel")[0] == 'stylesheet':
+            file_url = urljoin(url, file.attrs.get("href"))
+            files_to_download.append(file_url.split("?")[0])
+
+    if not os.path.exists(TEMP_DOWNLOADS_PATH):
+        os.makedirs(TEMP_DOWNLOADS_PATH)
+
+    # downloading files
+    for filename in files_to_download:
+        file = requests.get(filename)
+        with open (TEMP_DOWNLOADS_PATH + filename.split("/")[-1], 'wb') as result:
+            result.write(file.content)
