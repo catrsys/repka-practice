4022f48c7f42d2f4d3affb8ed98cd097b2badc88
unknown <suncheture@gmail.com>
2022-03-03 00:04:45 +0300

    Optimise function code Issue #2 Код функции изменен, для ускорения работы и упрощения понимания

--- extractor.py
+++ extractor.py
@@ -6,22 +6,30 @@ from urllib.parse import urljoin
 def download_static_files(url: str, dst: str) -> None:
     session = requests.Session()
     session.headers["User-Agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
-    html = session.get(url).content
+    response = session.get(url)
+    response.raise_for_status()
+    html = response.content
 
     soup = bs(html, "html.parser")
 
     files_to_download = list()
 
     # get the file links
-    for file in soup.find_all(["script", "link"]):
-        #JavaScript
-        if file.attrs.get("src"):
-            file_url = urljoin(url, file.attrs.get("src"))
-            files_to_download.append(file_url.split("?")[0])
-        #CSS
-        if file.attrs.get("href") and file.attrs.get("rel")[0] == 'stylesheet':
-            file_url = urljoin(url, file.attrs.get("href"))
-            files_to_download.append(file_url.split("?")[0])
+    for file in soup.find_all("script", src=True):
+        file_url = urljoin(url, file["src"])
+        try:
+            file_url = file_url[:file_url.index("?")]
+        except ValueError:
+            pass
+        files_to_download.append(file_url)
+
+    for file in soup.find_all("link", href=True, rel="stylesheet"):
+        file_url = urljoin(url, file["href"])
+        try:
+            file_url = file_url[:file_url.index("?")]
+        except ValueError:
+            pass
+        files_to_download.append(file_url)
 
     if not os.path.exists(dst):
         os.makedirs(dst)
