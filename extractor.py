56861100fc8cc074e24763a4566d645789c206d5
unknown <suncheture@gmail.com>
2022-03-02 23:49:05 +0300

    Change function arguments Issue #2 Изменены аргументы функции извлечения файлов

--- extractor.py
+++ extractor.py
@@ -3,9 +3,7 @@ import os
 from bs4 import BeautifulSoup as bs
 from urllib.parse import urljoin
 
-from constants import TEMP_DOWNLOADS_PATH
-
-def download_static_files(url: str) -> None:
+def download_static_files(url: str, dst: str) -> None:
     session = requests.Session()
     session.headers["User-Agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
     html = session.get(url).content
@@ -25,11 +23,11 @@ def download_static_files(url: str) -> None:
             file_url = urljoin(url, file.attrs.get("href"))
             files_to_download.append(file_url.split("?")[0])
 
-    if not os.path.exists(TEMP_DOWNLOADS_PATH):
-        os.makedirs(TEMP_DOWNLOADS_PATH)
+    if not os.path.exists(dst):
+        os.makedirs(dst)
 
     # downloading files
     for filename in files_to_download:
         file = requests.get(filename)
-        with open (TEMP_DOWNLOADS_PATH + filename.split("/")[-1], 'wb') as result:
+        with open (dst + filename.split("/")[-1], 'wb') as result:
             result.write(file.content)
