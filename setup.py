90acd14f507df9ea4e6fb640c51a8487588f07eb
Eduard Lazarev <seriousbanan@gmail.com>
2022-03-03 17:07:04 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="repka",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "beautifulsoup4==4.10.0",
+        "requests==2.27.1",
+    ],
+)
